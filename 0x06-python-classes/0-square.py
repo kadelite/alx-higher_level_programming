c6f6982f54758d00bb5657792992445e759706b6
kadelite <mechatronics.samson@gmail.com>
2022-12-21 05:57:01 -0800

    task peace

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+
+"""Define a class Square."""
+
+
 class Square:
-	    """Empty class that defines a square"""
-		    pass
+    """Represent a square."""
+    pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+
+"""Define a class Square."""
+
+
 class Square:
-    """Defines a square"""
-	def __init__(self, size):
-	    """Initialises the data"""
-		self.__size = size
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
