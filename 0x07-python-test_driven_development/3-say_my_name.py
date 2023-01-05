3f44ad8e01de3a50bb5c86042c2d36c04303d3cb
kadelite <mechatronics.samson@gmail.com>
2023-01-05 11:28:14 -0800

    task fix

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
 """
-Module 3-say_my_name
-prints name in the order of first_name and last_name
+This module is composed by a function prints a message
 """
 
+
 def say_my_name(first_name, last_name=""):
-    """
-    Prints my name is <first_name> <last_name>
+    """ Function that prints "My name is <first name> <last name>"
     Args:
-        first_name (str)
-        last_name (str, optional): Defaults as an empty string
+        first_name: first name
+        last_name: last name
+    Returns:
+        No return
     Raises:
-        TypeError: if either first_name or last_name is not string
+        TypeError: If first_name or last_name is not a string
     """
 
-    if not isinstance(first_name, str):
-        raise TypeError('first_name must be a string')
-    if not isinstance(last_name, str):
-        raise TypeError('last_name must be a string')
-    print("My name is {:s} {:s}".format(first_name, last_name))
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
