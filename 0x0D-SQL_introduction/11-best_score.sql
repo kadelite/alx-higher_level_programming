52fcd20c735eac7e813f324af7dcc533d75eb638
kadelite <mechatronics.samson@gmail.com>
2023-02-15 13:25:53 -0800

    task database

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,5 +1,3 @@
--- Displays the max temperature of each state ordered by state
-SELECT state, MAX(value) AS max_temp
-FROM temperatures
-GROUP BY state
-ORDER BY state;
+-- Script that lists all records with a score >= 10
+-- Query to lists all records with a score >= 10 in the table second_table of a database
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
