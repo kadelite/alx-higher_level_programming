f2a6a1187ee8fe5738773cfda7b1c6bfb1a49ef8
kadelite <mechatronics.samson@gmail.com>
2023-03-13 13:34:59 -0700

    task javascript

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
-const arg1 = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < arg1.length; i++) {
-  console.log(arg1[i]);
+const texts = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+for (let i = 0; i < texts.length; i++) {
+  console.log(texts[i]);
 }
